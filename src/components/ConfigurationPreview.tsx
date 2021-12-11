af06565871520c7686189aaf3b1adcf86c911fce
Petr Janik <peta.janik@email.cz>
2021-12-11 14:14:45 +0100

    Add confirmation dialog

--- src/components/ConfigurationPreview.tsx
+++ src/components/ConfigurationPreview.tsx
@@ -1,5 +1,19 @@
-import { FC } from 'react';
-import { Box, Card, CardActionArea, CardActions, CardContent, IconButton, Typography } from '@mui/material';
+import { FC, useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Typography,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Canvas from './Canvas';
 import { ParsedConfiguration } from '../models/Configuration';
@@ -23,38 +37,66 @@ const ConfigurationPreview: FC<Props> = ({ configuration, canDelete = false }) =
   const user = useLoggedInUser();
 
   const deleteConfiguration = async () => {
+    closeConfirmationDialog();
     if (user) {
       await deleteDoc(usersConfigurationDocument(user.uid, configuration.id));
     }
   };
 
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+
+  const openConfirmationDialog = () => {
+    setConfirmationDialogOpen(true);
+  };
+
+  const closeConfirmationDialog = () => {
+    setConfirmationDialogOpen(false);
+  };
+
   return (
-    <Card>
-      <CardActionArea
-        onClick={openConfiguration}
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 2 }}
-      >
-        <Canvas generation={configuration.initialGeneration} boardSize={configuration.boardSize} canvasWidth={250} />
-        <Box sx={{ alignSelf: 'flex-start' }}>
-          <CardContent>
-            <Typography variant="h5">{configuration.name}</Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {configuration.createdAt.toLocaleString()}
-            </Typography>
-            <Typography variant="subtitle2" color="text.secondary">
-              By: {configuration.authorName}
-            </Typography>
-          </CardContent>
-        </Box>
-      </CardActionArea>
-      {canDelete && (
-        <CardActions>
-          <IconButton color="error" title="Delete" onClick={deleteConfiguration}>
-            <Delete />
-          </IconButton>
-        </CardActions>
-      )}
-    </Card>
+    <>
+      <Dialog open={confirmationDialogOpen} onClose={closeConfirmationDialog}>
+        <DialogTitle>Are you sure?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Do you really want to delete this configuration?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={deleteConfiguration} color="error" variant="outlined" autoFocus>
+            Delete
+          </Button>
+          <Button onClick={closeConfirmationDialog} variant="outlined">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Card>
+        <CardActionArea
+          onClick={openConfiguration}
+          sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 2 }}
+        >
+          <Canvas generation={configuration.initialGeneration} boardSize={configuration.boardSize} canvasWidth={250} />
+          <Box sx={{ alignSelf: 'flex-start' }}>
+            <CardContent>
+              <Typography variant="h5">{configuration.name}</Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                {configuration.createdAt.toLocaleString()}
+              </Typography>
+              <Typography variant="subtitle2" color="text.secondary">
+                By: {configuration.authorName}
+              </Typography>
+            </CardContent>
+          </Box>
+        </CardActionArea>
+        {canDelete && (
+          <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <IconButton color="error" title="Delete" onClick={openConfirmationDialog}>
+              <Delete />
+            </IconButton>
+          </CardActions>
+        )}
+      </Card>
+    </>
   );
 };
 
