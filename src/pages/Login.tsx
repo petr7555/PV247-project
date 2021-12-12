52e954115fa13b1f05122f4f88f620010c3c08e9
Petr Janik <peta.janik@email.cz>
2021-12-12 09:43:27 +0100

    Move login closer to center

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Button, Paper, Stack, Typography } from '@mui/material';
+import { Button, Container, Paper, Stack, Typography } from '@mui/material';
 import { FC, FormEvent, useState } from 'react';
 import GoogleIcon from '@mui/icons-material/Google';
 import { logIn } from '../utils/firebase';
@@ -12,40 +12,49 @@ const Login: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Paper
-      component="form"
-      onSubmit={async (e: FormEvent) => {
-        e.preventDefault();
-        try {
-          await logIn();
-          navigate('/');
-        } catch (err) {
-          setSubmitError(getErrorMessage(err));
-        }
-      }}
+    <Container
       sx={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         flexDirection: 'column',
-        p: 4,
-        gap: 2,
+        alignItems: 'center',
+        pt: 8,
       }}
     >
-      <Typography variant="h4" component="h1" textAlign="center">
-        Log in
-      </Typography>
-      <Stack gap={1}>
-        <Button type="submit" variant="contained" startIcon={<GoogleIcon />}>
-          Sign in with Google
-        </Button>
-        {submitError && (
-          <Typography variant="caption" textAlign="right" sx={{ color: 'error.main' }}>
-            {submitError}
-          </Typography>
-        )}
-      </Stack>
-    </Paper>
+      <Paper
+        component="form"
+        onSubmit={async (e: FormEvent) => {
+          e.preventDefault();
+          try {
+            await logIn();
+            navigate('/');
+          } catch (err) {
+            setSubmitError(getErrorMessage(err));
+          }
+        }}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          p: 4,
+          gap: 2,
+        }}
+      >
+        <Typography variant="h4" component="h1" textAlign="center">
+          Log in
+        </Typography>
+        <Stack gap={1}>
+          <Button type="submit" variant="contained" startIcon={<GoogleIcon />}>
+            Sign in with Google
+          </Button>
+          {submitError && (
+            <Typography variant="caption" textAlign="right" sx={{ color: 'error.main' }}>
+              {submitError}
+            </Typography>
+          )}
+        </Stack>
+      </Paper>
+    </Container>
   );
 };
 
